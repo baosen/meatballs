4effb7182a58c88a4171f210ac276d61c04bcce0
Chabaluz <einar.antonsen@gmail.com>
2014-11-05 22:25:11 +0100

    Tried to create a table with resources

--- js/index.js
+++ js/index.js
@@ -3,3 +3,15 @@ function getResources() {
         createTable(data);
     });
 }
+
+function createTable(json) {
+    $('#resourceTable').empty();
+
+    for(var i = 0; i < json.length; i++) {
+        var res = json[i];
+        var tableString = "<tr>";
+        tableString += "<td>" + student.displayName + "</td>";
+        tableString += "</tr>";
+        $('resourceTable').append(tableString);
+    }
+}
