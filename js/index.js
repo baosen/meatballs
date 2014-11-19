df94f2d1e949662034d8ea72b84cbd80abdb0180
Einar Antonsen <einarant@ifi.uio.no>
2014-11-19 15:46:49 +0100

    Some tinkering

--- js/index.js
+++ js/index.js
@@ -1,17 +1,24 @@
 function getResources() {
-    $.get("/api/resources", function(data) {
+    console.log("getResources");
+    $.get("/api/resources.json", function(data) {
+        console.log(data);
         createTable(data);
     });
 }
 
 function createTable(json) {
-    $('#resourceTable').empty();
+    console.log("createTable");
+    //$('#resourceTable').empty();
 
     for(var i = 0; i < json.length; i++) {
         var res = json[i];
         var tableString = "<tr>";
-        tableString += "<td>" + student.displayName + "</td>";
+        tableString += "<td>" + res.singular + "</td>";
         tableString += "</tr>";
         $('resourceTable').append(tableString);
+        //document.getElementById("resourceTable").innerHTML =
+
     }
+
+    console.log("createTable done");
 }
