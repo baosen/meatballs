1dfa78e5661f01224d548e4100432f158f6c9f2e
Håvard Strand <haavstr@ifi.uio.no>
2014-11-20 13:50:45 +0100

    Attempted print of second level lists, does not print correctly

--- js/index.js
+++ js/index.js
@@ -6,17 +6,58 @@ function getResources() {
     });
 }
 
-function goToJson(link) {
+function goToList(link) {
     console.log("goToJson");
     $.get(link, function(data) {
         console.log(data);
-        createTable(data);
+        printList(data);
     });
 }
 
 function test() {
     console.log("Clicked a button-test-");
 }
+
+function printList(json){
+
+ console.log("printList");
+    $('#resourceTable').empty();
+
+    console.log("TEST");
+
+    var key, count = 0;
+    var list = Object.keys(json)[1];
+    for (key in list) {
+        if(list.hasOwnProperty(key)) {
+            var res = list[key];
+            var tableString = "<tr>";
+            //console.log(res.singular);
+            //console.log(key);
+            tableString += "<td>" + res.name + "</td>";
+            tableString += "<td>" + '<button onclick="goToList(\'' + res.href + '.json' + '\')">Go to</button></td>';
+            tableString += "</tr>";
+            $('#resourceTable').append(tableString);
+
+            count++;
+        }
+    }
+    console.log("count = " + count);
+    console.log(json.length);
+    /*for(var i = 0; i < count; i++) {
+        var res = json.resources[i];
+        var tableString = "<tr>";
+        //console.log(res.singular);
+        tableString += "<td>" + res.href + "</td>";
+        tableString += "<td>" + '<button onclick="test()">test</button></td>';
+        tableString += "<td>" + '<button onclick="goToJson(\'' + res.href + '\')">Go to</button></td>';
+        tableString += "</tr>";
+        $('#resourceTable').append(tableString);
+
+    }
+    */
+    console.log("createTable done");
+}   
+
 
 function createTable(json) {
     console.log("createTable");
@@ -32,8 +73,7 @@ function createTable(json) {
             //console.log(res.singular);
             //console.log(key);
             tableString += "<td>" + res.singular + "</td>";
-            tableString += "<td>" + '<button onclick="test()">test</button></td>';
-            tableString += "<td>" + '<button onclick="goToJson(\'' + res.href + '\')">Go to</button></td>';
+            tableString += "<td>" + '<button onclick="goToList(\'' + res.href + '.json' + '\')">Go to</button></td>';
             tableString += "</tr>";
             $('#resourceTable').append(tableString);
 
