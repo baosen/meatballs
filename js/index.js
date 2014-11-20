9bb7c0ae428660070ac7ea1b2c80d55329bd9611
Håvard Strand <haavstr@gmail.com>
2014-11-21 00:53:47 +0100

    Fixed function for printing of lists on second level of the Api. Still needs support for pages after page 1.

--- js/index.js
+++ js/index.js
@@ -20,18 +20,37 @@ function test() {
 
 function printList(json){
 
- console.log("printList");
+    console.log("printList");
+    console.log(json);
     $('#resourceTable').empty();
 
-    console.log("TEST");
+    /* Used when testing
+    console.log("json[0] = ");
+    console.log(Object.keys(json)[0]);
+    console.log("json[1] = ");
+    console.log(Object.keys(json)[1]);
+    console.log("list?");
+    console.log(json[Object.keys(json)[1]]);
+    */
+
+    //Print total, mainly for testing purposes
+    var info = "<tr><td>Total: " + json.pager.total + "</td></tr>";
+
+
+    $('#resourceTable').append(info);
 
     var key, count = 0;
-    var list = Object.keys(json)[1];
+ 
+    //Takes the name from the second key in the json and then accesses it to bind list to the array it contains
+    var nameOfList = Object.keys(json)[1];
+    var list = json[nameOfList];
+ 
+
     for (key in list) {
         if(list.hasOwnProperty(key)) {
             var res = list[key];
             var tableString = "<tr>";
-            //console.log(res.singular);
+            console.log(res.name);
             //console.log(key);
             tableString += "<td>" + res.name + "</td>";
             tableString += "<td>" + '<button onclick="goToList(\'' + res.href + '.json' + '\')">Go to</button></td>';
@@ -61,6 +80,7 @@ function printList(json){
 
 function createTable(json) {
     console.log("createTable");
+
     $('#resourceTable').empty();
 
     console.log("TEST");
